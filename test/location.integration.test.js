83cf2a4d284f207c0d041299d417ff6a34ef33b0
Ronnie Nyaga <ronnienyaga@gmail.com>
2017-05-23 12:23:23 +0300

    Added integration test for GET:/locations to fetch all locations

---
+++ test/location.integration.test.js
@@ -0,0 +1,41 @@
+"use strict";
+
+var request = require('supertest');
+var api = request('http://localhost:3001');
+var chai = require('chai');
+var expect = chai.expect;
+var should = chai.should;
+var app = require('../server.js');
+var mongoose = require('mongoose');
+var Location = mongoose.model('Location', 'LocationSchema');
+var agent = request.agent(app);
+
+describe('Location CRUD integration testing', function() {
+
+  describe('GET:/locations fetch all location', function() {
+
+    it('should return a 200 response', function (done) {
+			api.get('/api/locations')
+         .set('Accept', 'application/json')
+			   .expect(200, done);
+		});
+
+    it('should return locations as an array', function(done) {
+      api.get('/api/locations')
+         .set('Accept', 'application/json')
+         .end(function(err, res) {
+           expect(res.body.location).to.be.a('array');
+           done();
+         });
+    });
+
+    it('should return status as true', function(done) {
+      api.get('/api/locations')
+         .set('Accept', 'application/json')
+         .end(function(err, res) {
+           expect(res.body.status).to.be.true;
+           done();
+         });
+    });
+  }
+}
