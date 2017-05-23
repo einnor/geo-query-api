65b4b04f3190bd1cffe3030e15dcb2780714619f
Ronnie Nyaga <ronnienyaga@gmail.com>
2017-05-23 09:06:42 +0300

    Added unit test for Location.find() method

---
+++ test/location.unit.test.js
@@ -0,0 +1,31 @@
+"use strict";
+
+var sinon = require('sinon');
+var chai = require('chai');
+var expect = chai.expect;
+
+var mongoose = require('mongoose');
+require('sinon-mongoose');
+
+// import our location model for unit testing
+var Location = mongoose.model('Location', 'LocationSchema');
+
+describe('Location Unit Testing', function() {
+
+  describe('find() method to fetch all the locations', function() {
+
+    // test will pass if the method find() exists and returns an array of locations
+    it('should return an array of locations', function(done) {
+      var LocationMock = sinon.mock(Location);
+      var expectedResult = [];
+      LocationMock.expects('find').yields(null, expectedResult);
+
+      Location.find(function(err, result) {
+        LocationMock.verify();
+        LocationMock.restore();
+        expect(result).to.be.a('array');
+        done();
+      });
+    });
+  });
+});
