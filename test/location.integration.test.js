3084c9bf7daf4fc230c03db9f8826fdd88653576
Ronnie Nyaga <ronnienyaga@gmail.com>
2017-05-23 12:47:29 +0300

    Added integration test GET:/locations/:id fetch a location by id

--- test/location.integration.test.js
+++ test/location.integration.test.js
@@ -8,6 +8,7 @@ var should = chai.should;
 var app = require('../server.js');
 var mongoose = require('mongoose');
 var Location = mongoose.model('Location', 'LocationSchema');
+var faker = require('faker');
 
 describe('Location CRUD integration testing', function() {
 
@@ -30,6 +31,59 @@ describe('Location CRUD integration testing', function() {
 
     it('should return status as true', function(done) {
       api.get('/api/locations')
+         .set('Accept', 'application/json')
+         .end(function(err, res) {
+           expect(res.body.status).to.be.true;
+           done();
+         });
+    });
+  });
+
+
+  describe('GET:/locations/:id fetch a location by id', function() {
+
+    var response = {};
+
+    before(function(done) {
+      var newLocation = {
+        name: faker.town,
+        loc: [faker.longitude, faker.latitude]
+      };
+      api.post('/api/locations')
+         .set('Accept', 'application/x-www-form-urlencoded')
+         .send(newLocation)
+         .expect('Content-Type', '/json/')
+         .expect(200)
+         .end(function(err, res) {
+           response = res.body;
+            done();
+          });
+    });
+
+    it('should return a 200 response', function (done) {
+			api.get('/api/locations/' + response.location._id)
+         .set('Accept', 'application/json')
+			   .expect(200, done);
+		});
+
+    it('should return an object with keys and values', function(done) {
+      api.get('/api/locations/' + response.location._id)
+         .set('Accept', 'application/json')
+         .end(function(err, res) {
+           expect(res.body).to.have.property('status');
+           expect(res.body.status).to.not.equal(null);
+           expect(res.body.location).to.be.a('object');
+           expect(res.body.location).to.have.property('name');
+           expect(res.body.location.name).to.not.equal(null);
+           expect(res.body.location).to.have.property('loc');
+           expect(res.body.location.loc).to.not.equal(null);
+           expect(res.body.location.loc).to.be.a('array');
+           done();
+         });
+    });
+
+    it('should return status as true', function(done) {
+      api.get('/api/locations/' + response.location._id)
          .set('Accept', 'application/json')
          .end(function(err, res) {
            expect(res.body.status).to.be.true;
